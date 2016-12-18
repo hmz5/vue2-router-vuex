df8b76fefd9a5e566e7e599863e22443135ad43c
heminzhi <541058460@qq.com>
2016-12-18 14:22:02 +0800

    add

---
+++ src/vuex/index.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import * as actions from './actions'
+import * as getters from './getters'
+import game from './modules/game'
+import shop from './modules/shop'
+// console log vuex
+import createLogger from 'vuex/dist/logger';
+
+
+Vue.use(Vuex)
+
+const debug = true;
+
+export default new Vuex.Store({
+  actions,
+  getters,
+  modules: {
+    game,
+    shop
+  },
+  // 是否开启调试模式
+  strict: true,
+  // 插件
+  plugins: debug ? [createLogger()] : []
+})
